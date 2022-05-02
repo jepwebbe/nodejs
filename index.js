b973024fc2f99d9abb2e87f294a7cfc8297017da
jepwebbe <94107611+jepwebbe@users.noreply.github.com>
2022-05-02 10:18:33 +0200

    1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import http from 'http';
 
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('No Hello World!');
+    response.write('Yes No Hello World!');
     response.end();
 }).listen(4000)
